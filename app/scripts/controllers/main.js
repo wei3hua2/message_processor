174abb9f48904e17fc523f76ea5acba91020ca82
James Chong <email@jameschong.me>
2013-11-21 20:29:16 +0800

    added menu and ui adjustment

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,9 @@
 
 angular.module('messageProcessorApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+        $scope.messages = [
+            {type:'bd',gift:'iphone',processed:false},
+            {type:'congrat',babyName:'Ted',birthDate:'01-03-2011',processed:false}
+        ];
   });
