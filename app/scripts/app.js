7826862793b09e6af90fb18125a471ed9021825c
James Chong <email@jameschong.me>
2013-11-22 20:51:25 +0800

    added processor controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,10 @@ angular.module('messageProcessorApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/process', {
+        templateUrl: 'views/process_msg.html',
+        controller: 'ProcessMsgCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
 angular.module('messageProcessorApp')
-  .controller('MainCtrl', function ($scope) {
-
+  .controller('MainCtrl', ['$scope',function ($scope) {
         $scope.messages = [
             {type:'bd',gift:'iphone',processed:false},
             {type:'congrat',babyName:'Ted',birthDate:'01-03-2011',processed:false}
         ];
-  });
+  }])
+    .controller('ProcessMsgCtrl',['$scope',function($scope){
+        $scope.unprocessedMsg = [{id:'1',type:'bd',gift:'iphone',processed:false},
+            {id:'2',type:'congrat',babyName:'Ted',birthDate:'01-03-2011',processed:false}];
+    }])
+  .directive('displayItemList',[function(){
+        return {
+            templateUrl: 'views/directive/itemList.html',
+            restrict:'A',
+            scope:{
+                item:'='
+            },
+            link: function(scope, element, attrs){
+                element.find('.open-btn').click(function(){
+                    console.log(scope.item);
+
+                    element.find('.modal').modal();
+
+                })
+            }
+        }
+  }]);
